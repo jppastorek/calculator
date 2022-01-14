55051eb9f5b094c4ea98448c3bb1903fa6a7f7bc
jppastorek <jppastorek@gmail.com>
2022-01-14 08:15:02 -0600

    Added number button functionality.

--- scripts.js
+++ scripts.js
@@ -18,7 +18,7 @@ const btnAdd = document.querySelector(".btn-add");
 const btnEquals = document.querySelector(".btn-equals");
 const btnDecimal = document.querySelector(".btn-decimal");
 const buttonsNumber = document.querySelectorAll(".btn-number");
-const displayText = document.querySelector(".display-text").innerText;
+const displayText = document.querySelector(".display-text");
 let currentDisplay = "0";
 let firstNumber;
 let secondNumber;
@@ -31,17 +31,35 @@ let secondNumber;
 
 
 function updateDisplay() {
-    displayText = currentDisplay;
+    displayText.innerText = currentDisplay;
 }
 
 
-
+updateDisplay();
 
 
 
 
 
 ///////////////////////////////EVENT LISTENERS/////////////////////
+
+//max length of numbers is 12
+
+btnClear.addEventListener("click", () => {
+    currentDisplay = "0";
+    updateDisplay();
+});
+
+//REFACTOR THIS WITH A FOR LOOP
+
+btn0.addEventListener("click", () => {
+    if (displayText.innerText != "0") {
+        currentDisplay += "0";
+    } else {
+        currentDisplay = "0";
+    }
+    updateDisplay();
+});
 
 btn1.addEventListener("click", () => {
     if (displayText.innerText != "0") {
@@ -50,4 +68,76 @@ btn1.addEventListener("click", () => {
         currentDisplay = "1";
     }
     updateDisplay();
-})
+});
+
+btn2.addEventListener("click", () => {
+    if (displayText.innerText != "0") {
+        currentDisplay += "2";
+    } else {
+        currentDisplay = "2";
+    }
+    updateDisplay();
+});
+
+btn3.addEventListener("click", () => {
+    if (displayText.innerText != "0") {
+        currentDisplay += "3";
+    } else {
+        currentDisplay = "3";
+    }
+    updateDisplay();
+});
+
+btn4.addEventListener("click", () => {
+    if (displayText.innerText != "0") {
+        currentDisplay += "4";
+    } else {
+        currentDisplay = "4";
+    }
+    updateDisplay();
+});
+
+btn5.addEventListener("click", () => {
+    if (displayText.innerText != "0") {
+        currentDisplay += "5";
+    } else {
+        currentDisplay = "5";
+    }
+    updateDisplay();
+});
+
+btn6.addEventListener("click", () => {
+    if (displayText.innerText != "0") {
+        currentDisplay += "6";
+    } else {
+        currentDisplay = "6";
+    }
+    updateDisplay();
+});
+
+btn7.addEventListener("click", () => {
+    if (displayText.innerText != "0") {
+        currentDisplay += "7";
+    } else {
+        currentDisplay = "7";
+    }
+    updateDisplay();
+});
+
+btn8.addEventListener("click", () => {
+    if (displayText.innerText != "0") {
+        currentDisplay += "8";
+    } else {
+        currentDisplay = "8";
+    }
+    updateDisplay();
+});
+
+btn9.addEventListener("click", () => {
+    if (displayText.innerText != "0") {
+        currentDisplay += "9";
+    } else {
+        currentDisplay = "9";
+    }
+    updateDisplay();
+});
